bf34f098140ff6898ef120d545497b217487db3d
Eugene  Choi <eugeneiohc@hotmail.com>
2018-05-14 20:57:48 -0400

    final commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+function validate(){
+	var zipCode = $("#zip").val();
+	if(isNaN(zipCode) || zipCode.length != 5){
+		alert("Zipcode is not valid (5 digit Zip Code)");
+		return false;
+	}
+}
+
+var slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
